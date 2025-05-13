86544da0710a5e354f00f03a85b09a8f78e57dff
FredericS-2024 <fredonetspike@gmail.com>
2025-05-13 06:00:30 +0200

    Ajout page d'inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
-    new Route("/Signin", "Connexion", "/pages/Signin.html")
+    new Route("/Signin", "Connexion", "/pages/Signin.html"),
+    new Route("/Signup", "Inscription", "/pages/Signup.html"),
 ]
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
